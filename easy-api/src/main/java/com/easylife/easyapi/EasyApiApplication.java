24268fa2c41773b21909a57e016029532a178275
valdineia-martins <valdineia.dev@gmail..com>
2023-08-21 19:23:57 -0300

    iniciado

---
+++ easy-api/src/main/java/com/easylife/easyapi/EasyApiApplication.java
@@ -0,0 +1,13 @@
+package com.easylife.easyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyApiApplication.class, args);
+	}
+
+}
---
+++ easy-api/src/test/java/com/easylife/easyapi/EasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easylife.easyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
