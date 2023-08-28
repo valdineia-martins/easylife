94854a08e56e813c9b5c33bb8849d7cf71813067
valdineia-martins <revige8763@xgh6.com>
2023-08-28 18:42:56 -0300

    Endpoint lista e buscar pelo código

--- easy-api/src/main/java/com/easylife/easyapi/repository/PessoaRepository.java
+++ easy-api/src/main/java/com/easylife/easyapi/repository/PessoaRepository.java
@@ -4,6 +4,8 @@ import com.easylife.easyapi.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 }
